034d7a107414faa2d04ab9a3936538e1a9d01938
jacklin953107 <jacklin953107@gmail.com>
2017-02-07 19:59:48 +0800

    CRUD

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,47 @@
+class EventsController < ApplicationController
+  #before_action :authenticate_user!
+  before_action :set_event, :only => [ :show, :edit, :update, :destroy]
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event =Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+
+    redirect_to :action => :index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @event.update(event_params)
+
+    redirect_to :action => :show, :id => @event
+  end
+
+  def destroy
+    @event.destroy
+
+    redirect_to :action => :index
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :content)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+end
